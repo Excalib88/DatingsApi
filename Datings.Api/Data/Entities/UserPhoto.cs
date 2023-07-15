c41176570fa23ab4497f8c4c9811285be38d75d8
Damir Mansurov <d.mansurov@mandarin.io>
2023-07-15 13:06:44 +0300

    modified user photo

--- Datings.Api/Data/Entities/UserPhoto.cs
+++ Datings.Api/Data/Entities/UserPhoto.cs
@@ -4,6 +4,7 @@ public class UserPhoto: BaseEntity
 {
     //base64
     public string? Data { get; set; }
+    public int Order { get; set; }
     public long? UserId { get; set; }
     public ApplicationUser? User { get; set; }
 }
---
+++ Datings.Api/Data/Migrations/20230715100500_OrderUserPhoto.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExcalibQuestions.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderUserPhoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "UserPhoto",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "UserPhoto");
+        }
+    }
+}
